6c5a5ac2aae71284a54c63dca53ab40956845942
Rene Balda <rbalda@espol.edu.ec>
2014-01-14 03:08:56 -0500

    subiendo proyecto

---
+++ parserXml.hs
@@ -0,0 +1,16 @@
+import Data.Char
+import Data.List
+import System.IO
+import Data.List.Split
+
+deleteUnusedTags :: String ->String
+deleteUnusedTags substring string = 
+
+readXml src = do 
+				xml <- readFile src
+				putStr xml
+				
+				
+
+				
+					
